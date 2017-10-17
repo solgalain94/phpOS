8e5a00cccfefec1d7758dc710acb25a02daeb9e2
solgalain94 <solgalain@gmail.com>
2017-10-17 13:26:23 -0300

    First commit

---
+++ hola.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Hola Mundo</p>'; ?>
+ </body>
+<<<<<<< HEAD
+</html>
+=======
+</html>
+>>>>>>> b7bd34bb50125491d29d29a0f1d947cff06d7dca
